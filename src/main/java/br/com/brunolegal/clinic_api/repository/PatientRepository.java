c0fc6e7c2b1387be4d3b8b0f30e7b76b1eea71dc
Bruno Antônio Joanucci <bruno.a.joanucci@gmail.com>
2025-10-21 17:21:10 -0300

    fix: add missing @Repository annotation

--- src/main/java/br/com/brunolegal/clinic_api/repository/PatientRepository.java
+++ src/main/java/br/com/brunolegal/clinic_api/repository/PatientRepository.java
@@ -2,7 +2,9 @@ package br.com.brunolegal.clinic_api.repository;
 
 import br.com.brunolegal.clinic_api.domain.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
     //creates a query SELECT * FROM patients WHERE email = ?1
     boolean existsByEmail(String email);
