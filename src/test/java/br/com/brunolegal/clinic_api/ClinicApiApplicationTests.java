cdae84e6c88cf754120890df807e747cb4b40026
Bruno Antônio Joanucci <bruno.a.joanucci@gmail.com>
2025-10-21 21:13:57 -0300

    fix: configure Spring Profiles for h2 and it tests

--- src/test/java/br/com/brunolegal/clinic_api/ClinicApiApplicationTests.java
+++ src/test/java/br/com/brunolegal/clinic_api/ClinicApiApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.brunolegal.clinic_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("h2")
 class ClinicApiApplicationTests {
 
 	@Test
--- src/test/java/br/com/brunolegal/clinic_api/controller/PatientControllerTest.java
+++ src/test/java/br/com/brunolegal/clinic_api/controller/PatientControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @Testcontainers
+@ActiveProfiles("it")
 public class PatientControllerTest {
     @Container
     static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine");
